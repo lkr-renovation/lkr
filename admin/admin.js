c2a053d90440d8df988516132a53ea165ff8ce79
lkr-renovation <nonmistressatelepalle@gmail.com>
2025-10-27 15:42:27 +0100

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -20,7 +20,7 @@
   // ============================================
   
   const LKR_CONFIG = {
-  adminPasswordHash: "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+  adminPasswordHash: "21bc62c695e168a7576b91e2965bc1a34f8def2b5fb55b0adbe50e1065863600",
   
   // Endpoint API (Vercel Serverless Functions)
   apiTranslate: '/api/translate',
